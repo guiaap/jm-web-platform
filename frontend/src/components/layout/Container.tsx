f9004d6402c7c59583953744a4a02eb928b02bf4
Guilherme Amaral <guilhermeamaral325@gmail.com>
2026-09-13 11:54:47 -0300

    feat: add Container layout component

---
+++ frontend/src/components/layout/Container.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+
+interface ContainerProps {
+    children: ReactNode;
+}
+
+function Container({ children }: ContainerProps) {
+
+    return (
+        <div className="max-w-(--container-content) mx-auto">
+            { children }
+        </div>
+    );
+}
+
+export default Container;
